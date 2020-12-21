becae608cc93b1c0f05b2d988b526fe5ce5c8b84
bezrodnva07@yandex.ru <bezrodnva07@yandex.ru>
2020-12-21 19:54:47 +0800

    4

--- curs/Form1.cs
+++ curs/Form1.cs
@@ -13,70 +13,32 @@ namespace curs
     public partial class Form1 : Form
     {
         List<Particle> particles = new List<Particle>();
+        Emitter emitter = new Emitter();
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-
-
+  
         }
 
-        private void UpdateState()
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1;
-                if (particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = picDisplay.Image.Width / 2;
-                    particle.Y = picDisplay.Image.Height / 2;
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                }
-            }
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 50)
-                {
-                    var particle = new ParticleColorful();
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = picDisplay.Image.Width / 2;
-                    particle.Y = picDisplay.Image.Height / 2;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-        private void Render(Graphics g)
-        {
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState();
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White);
                 g.Clear(Color.Black);
-                Render(g);
+                emitter.Render(g);
             }
             picDisplay.Invalidate();
         }
+
     }
 }
