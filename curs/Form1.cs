45039c8d9e8a43e10cd09716d6ecceb61b016af5
bezrodnva07@yandex.ru <bezrodnva07@yandex.ru>
2020-12-21 18:27:24 +0800

    начало

--- curs/Form1.cs
+++ curs/Form1.cs
@@ -15,6 +15,7 @@ namespace curs
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
         }
     }
 }
